6776b353d637c4d2fdd8a8d2afaa6b2ff055cafe
LESLIE KOFI BROBBEY <51799467+KofiLeslie@users.noreply.github.com>
2026-01-16 01:28:23 +0000

    Remove API documentation comment from Controller and add api-docs.json for Dodo Notifications API

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,13 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-/**
- * @OA\Info(
- *      version="1.0.0",
- *      title="Dodo Notifications API",
- *      description="API documentation for the Dodo notification system",
- * )
- */
+
 abstract class Controller
 {
     //
